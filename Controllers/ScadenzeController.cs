505e7b97c215d104618f3fdf23deae21d3d76b88
malbasini <malbasini@gmail.com>
2021-03-13 16:00:42 +0100

    Modifica ScadenzeController

--- Controllers/ScadenzeController.cs
+++ Controllers/ScadenzeController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Scadenzario.Models.Services.Application;
+using Scadenzario.Models.ViewModels;
 
 namespace Scadenzario.Controllers
 {
@@ -6,11 +9,19 @@ namespace Scadenzario.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var scadenzeService = new ScadenzeService();
+            List<ScadenzeViewModel> scadenze = new();
+            scadenze = scadenzeService.GetScadenze();
+            ViewData["Title"]="Lista Scadenze";
+            return View(scadenze);
         }
         public IActionResult Detail(int id)
         {
-            return View();
+            var scadenzeService = new ScadenzeService();
+            ScadenzeViewModel scadenze;
+            scadenze = scadenzeService.GetScadenza(id);
+            ViewData["Title"]="Dettaglio Scadenza "+ id.ToString();
+            return View(scadenze);
         }
     }
 }
