93306f414c98ca4c70fe42a6867b25094966fd89
malbasini <malbasini@gmail.com>
2021-03-24 05:04:52 +0100

    Aggiornato file stratup

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace Scadenzario
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddTransient<IScadenzeService,ScadenzeService>();
+            services.AddTransient<IScadenzeService,EFCoreScadenzaService>();
             services.AddTransient<IBeneficiariService,EFCoreBeneficiarioService>();
             services.AddDbContextPool<MyScadenzaDbContext>(optionsBuilder=>{
                  optionsBuilder.UseSqlServer("Server=LAPTOP-SMBSSRS2\\SQLEXPRESS;Database=Scadenzario;Trusted_Connection=True;User ID=LAPTOP-SMBSSRS2\\marco;");
