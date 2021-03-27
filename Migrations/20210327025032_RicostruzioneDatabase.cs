38960ce749193bdd88604782d44191380f04bcb9
malbasini <malbasini@gmail.com>
2021-03-27 03:52:43 +0100

    Ricostruzione Database

---
+++ Migrations/20210327025032_RicostruzioneDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scadenzario.Migrations
+{
+    public partial class RicostruzioneDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
