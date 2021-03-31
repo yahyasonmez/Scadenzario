56560e6369971054126bc11780f884e67cb027fa
malbasini <malbasini@gmail.com>
2021-03-31 17:33:27 +0200

    Commit

--- obj/Debug/net5.0/Scadenzario.AssemblyInfo.cs
+++ obj/Debug/net5.0/Scadenzario.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("Scadenzario")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scadenzario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
